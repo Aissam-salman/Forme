a05a236a0adbf03341acabf380696d96f7b9856d
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-16 16:22:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Forme 
+
+- Java spring boot application 
+- JS with ReactJS
+- MongoDB and Mysql 
+- ...
+
+
+## Getting started 
+```shell
+
+./mvnw spring-boot:start
+
+```
+
---
+++ src/main/java/com/forme/app/FormeApplication.java
@@ -0,0 +1,13 @@
+package com.forme.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormeApplication.class, args);
+    }
+
+}
