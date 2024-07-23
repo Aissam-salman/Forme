91ca713d3436fda0c1644de1cbfc1ff5e4e9ce9a
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-23 14:35:02 +0200

    fix(model): - correct entity relation

--- src/main/java/com/forme/app/user/model/Candidate.java
+++ src/main/java/com/forme/app/user/model/Candidate.java
@@ -21,6 +21,7 @@ import java.util.List;
 @Data
 @SuperBuilder
 @NoArgsConstructor
+@AllArgsConstructor
 @PrimaryKeyJoinColumn(name = "id")
 public class Candidate extends User {
     private Date birthday;
