d210b1bcc10e723a5a275cff51ac25f594b57a96
Aissam-salman <nasawi.lmj@gmail.com>
2024-09-19 11:02:18 +0200

    feat(swagger): I add swagger

--- src/main/java/com/forme/app/config/SecurityConfig.java
+++ src/main/java/com/forme/app/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/v1/auth/**", "/**").permitAll();
+                    auth.requestMatchers("/swagger-ui/**").permitAll();
                 })
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
